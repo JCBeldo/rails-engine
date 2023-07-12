aecc5c3751cd443cf39cb3b5b7d8ffd04a82d3a9
Julian Beldotti <julian.beldotti@gmail.com>
2023-07-11 20:51:07 -0500

    Feat: Add models, controllers and serializers to make postman harness pass

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    render json: ItemSerializer.new(Item.all)
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render(json: ItemSerializer.new(Item.create!(item_params)), status: 201)
+    else
+      render :status => 404
+    end
+  end
+
+  def destroy
+    render json: Item.destroy(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :items do
+        resources :merchant, only: [:index], controller: "item_merchant"
+      end
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], controller: "merchant_items"
+      end
+    end
+  end
 end
