698ea6b08da681f8cbe0d11a560a2df22def1440
Julian Beldotti <julian.beldotti@gmail.com>
2023-07-12 00:29:07 -0500

    Fix: Rubocop formatting

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items do
-        resources :merchant, only: [:index], controller: "item_merchant"
+        resources :merchant, only: [:index], controller: 'item_merchant'
       end
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index], controller: "merchant_items"
+        resources :items, only: [:index], controller: 'merchant_items'
       end
     end
   end
